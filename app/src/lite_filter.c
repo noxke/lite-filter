8c496cfdc86009f5c449c63bab5afe9988894066
noxke <zpengkee@gmail.com>
2024-10-28 15:05:46 +0000

    user space update

--- app/src/lite_filter.c
+++ app/src/lite_filter.c
@@ -6,6 +6,7 @@
 
 #include "netlink_msg.h"
 #include "netlink_utils.h"
+#include "filter_rule_utils.h"
 #include "log_utils.h"
 
 int service_init() {
@@ -14,8 +15,8 @@ int service_init() {
         goto _service_netlink_init;
     }
 
-    // 设置日志消息处理器
-    netlink_set_msg_handler(NL_MSG_LOG, (void *)log_msg_handler);
+    // 设置过滤器配置
+    netlink_set_msg_handler(NL_MSG_CONF, (void *)nl_msg_config_handler);
 
     goto _service_all_init;
 
@@ -33,21 +34,6 @@ int service_main() {
     if (service_init() != 0) {
         return -1;
     }
-
-    struct nl_msg_struct *rep_msg;
-    char s[] = "11111111111111111111111111111111";
-    int s_size;
-    s_size = strlen(s);
-    rep_msg = malloc(NL_MSG_SIZE(s_size));
-    rep_msg->msg_type = NL_MSG_RAW;
-    rep_msg->msg_size = NL_MSG_SIZE(s_size);
-    if (rep_msg == NULL) {
-        return -1;
-    }
-    memcpy(NL_MSG_DATA(rep_msg), s, s_size);
-    nl_send_msg(rep_msg);
-    free(rep_msg);
-    sleep(3);
 
     service_exit();
     return 0;
