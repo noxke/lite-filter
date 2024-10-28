14b0b22a996e4dd3af694bc98c5a81e156da0e7c
noxke <zpengkee@gmail.com>
2024-10-28 15:01:27 +0000

    change log to file in kernel space

--- module/include/log_utils.h
+++ module/include/log_utils.h
@@ -12,11 +12,20 @@
 #define LOG_FILENAME_SIZE 256
 #define LOG_MSG_SIZE 1000
 
+typedef struct {
+    int config_type;
+    int log_level;
+    int log_kprint_level;
+    char log_file[LOG_FILENAME_SIZE];
+}LogConfig;
+
+extern int log_level;
+extern int log_kprint_level;
+extern char log_file[LOG_FILENAME_SIZE];
+
 extern void async_log(int log_level, const char *format, ...);
 
-extern void set_log_sender(void *logger);
-extern void set_log_level(int level);
-extern void set_log_kprint_level(int level);
+extern void log_config(LogConfig *conf);
 
 extern int log_utils_init(void);
 extern void log_utils_exit(void);
