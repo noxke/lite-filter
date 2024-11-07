b8453fbe2c825ec0af02072c63021f0561b70b41
noxke <zpengkee@gmail.com>
2024-11-07 21:41:03 +0000

    done config msg handler from app

--- module/src/filter_rule_utils.c
+++ module/src/filter_rule_utils.c
@@ -7,7 +7,11 @@
 #include <linux/tcp.h>
 #include <linux/icmp.h>
 #include <linux/icmpv6.h>
+#include <linux/mutex.h>
 
+#include "log_utils.h"
+#include "netlink_msg.h"
+#include "netlink_utils.h"
 #include "module_utils.h"
 #include "netfilter_hook.h"
 #include "filter_rule_utils.h"
@@ -104,6 +108,7 @@ FilterNodeV4 *filter_rule_insert_v4(FilterNodeV4 *rule_link, int index, FilterRu
     }
     return new_node;
 }
+
 FilterNodeV4 *filter_rule_remove_v4(FilterNodeV4 *rule_link, int index) {
     FilterNodeV4 *prev;
     FilterNodeV4 *rm_node;
@@ -131,13 +136,13 @@ FilterNodeV4 *filter_rule_remove_v4(FilterNodeV4 *rule_link, int index) {
     return rule_link;
 }
 
-void filter_rule_clear_v4(RuleConfig *conf) {
+void filter_rule_clear_v4(FilterNodeV4 *rule_link) {
     FilterNodeV4 *rm_node;
     FilterNodeV4 *next;
-    if (conf->hook_chain < NF_HOOK_PREROUTING || conf->hook_chain > NF_HOOK_POSTROUTING) {
+    if (rule_link == NULL) {
         return;
     }
-    next = hook_rule_link[conf->hook_chain];
+    next = rule_link;
     while (next != NULL) {
         rm_node = next;
         next = rm_node->next;
@@ -145,25 +150,78 @@ void filter_rule_clear_v4(RuleConfig *conf) {
     }
 }
 
-void filter_rule_dump_v4(RuleConfig *conf) {
-    FilterNodeV4 *rm_node;
+void filter_rule_dump_v4(FilterNodeV4 *rule_link, int hook_chain, const char *tmpfile) {
     FilterNodeV4 *next;
-    if (conf->hook_chain < NF_HOOK_PREROUTING || conf->hook_chain > NF_HOOK_POSTROUTING) {
+    struct nl_msg_struct *msg;
+    RuleConfig *msg_conf;
+    int idx = 0;
+    struct file *fp;
+    if (tmpfile == NULL) {
         return;
     }
+    fp = filp_open(tmpfile, O_WRONLY | O_CREAT, 0600);
+    if (IS_ERR(fp)) {
+        return;
+    }
+    next = rule_link;
+    msg = (struct nl_msg_struct *)kmalloc(NL_MSG_SIZE(sizeof(RuleConfig)), GFP_KERNEL);
+    if (msg == NULL) {
+        return;
+    }
+    msg->msg_type = NL_MSG_CONF;
+    msg->msg_size = NL_MSG_SIZE(sizeof(RuleConfig));
+    msg_conf = (RuleConfig *)NL_MSG_DATA(msg);
+    msg_conf->config_type = CONF_RULE_DUMP;
+    msg_conf->hook_chain = hook_chain;
+    while (next != NULL) {
+        msg_conf->index = idx;
+        kernel_write(fp, &(next->rule), sizeof(FilterRuleV4), &fp->f_pos);
+        next = next->next;
+        idx += 1;
+    }
+    filp_close(fp, NULL);
+    // 发送-1结束
+    msg_conf->index = idx;
+    memset(&(msg_conf->rule), 0, sizeof(FilterRuleV4));
+    nl_send_msg(msg);
+    kfree(msg);
 }
 
 void filter_rule_config(RuleConfig *conf) {
+    int index;
+    FilterRuleV4 *rule;
+    FilterNodeV4 *rule_link;
+    const char *chain_name;
+    struct mutex *chain_mutex;
+    if (conf->hook_chain <= NF_HOOK_NONE || conf->hook_chain >= NF_HOOK_MAX) {
+        return;
+    }
+    index = conf->index;
+    rule = &(conf->rule);
+    rule_link = nf_hook_table[conf->hook_chain].rule_link;
+    chain_name = nf_hook_table[conf->hook_chain].chain_name;
+    chain_mutex = &(nf_hook_table[conf->hook_chain].chain_mutex);
+    mutex_lock(chain_mutex);
     switch (conf->config_type) {
         case CONF_RULE_CLEAR:
-            filter_rule_clear_v4(conf);
+            async_log(LOG_WARNING, "Clear rules in %s", chain_name);
+            filter_rule_clear_v4(rule_link);
+            nf_hook_table[conf->hook_chain].rule_link = NULL;
             break;
         case CONF_RULE_INSERT:
+            async_log(LOG_WARNING, "Insert rule to %s idx=%d: %s", chain_name, index, conf->rule_str);
+            rule_link = filter_rule_insert_v4(rule_link, index, rule);
+            nf_hook_table[conf->hook_chain].rule_link = rule_link;
             break;
         case CONF_RULE_REMOVE:
+            async_log(LOG_WARNING, "Remove rule from %s idx=%d: %s", chain_name, index, conf->rule_str);
+            rule_link = filter_rule_remove_v4(rule_link, index);
+            nf_hook_table[conf->hook_chain].rule_link = rule_link;
             break;
         case CONF_RULE_DUMP:
-            filter_rule_dump_v4(conf);
+            async_log(LOG_WARNING, "Dump rules in %s", chain_name);
+            filter_rule_dump_v4(rule_link, conf->hook_chain, conf->rule_str);
             break;
     }
+    mutex_unlock(chain_mutex);
 }
