ef092484dae13a43ad685004b843cb9e225e4df5
noxke <zpengkee@gmail.com>
2024-11-07 21:40:03 +0000

    Optimizing netfilter hook functions

--- module/include/netfilter_hook.h
+++ module/include/netfilter_hook.h
@@ -2,9 +2,19 @@
 #define __NETFILTER_HOOK_H__
 // netfilter接口
 
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include "linux/mutex.h"
 #include "filter_rule_utils.h"
 
-extern FilterNodeV4 *hook_rule_link[5];
+struct nf_hook_table_struct {
+    FilterNodeV4 *rule_link;
+    struct mutex chain_mutex;
+    const char *chain_name;
+    struct nf_hook_ops ops;
+};
+
+extern struct nf_hook_table_struct nf_hook_table[NF_HOOK_MAX];
 
 extern int nf_hook_init(void);
 extern void nf_hook_exit(void);
